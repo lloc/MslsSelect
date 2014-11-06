a31ea29f401c7dc7e8b04dc604aaf1af30e6737d
lloc <re@lloc.de>
2014-11-06 12:34:14 +0100

    Version 0.1

---
+++ MslsSelect.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+Plugin Name: MslsSelect
+Plugin URI: https://github.com/lloc/MslsSelect
+Description: Transforms the output of the Multisite Language Switcher to an HTML select
+Version: 0.1
+Author: Dennis Ploetner
+Author URI: http://lloc.de/
+*/
+
+/*
+Copyright 2014  Dennis Ploetner  (email : re@lloc.de)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * MslsSelect Class
+ * @package mslsselect
+ */
+class MslsSelect {
+
+	const VERSION = '0.1';
+
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts',   array( $this, 'enqueue_scripts' ) );
+		add_filter( 'msls_output_get_tags', array( $this, 'get_tags' ) );
+		add_filter( 'msls_output_get',      array( $this, 'output_get' ), 10, 3 );
+	}
+
+	public function enqueue_scripts() {
+		wp_enqueue_script(
+			'mslsselect',
+			plugins_url( '/js/mslsselect.js' , __FILE__ ),
+			array( 'jquery' ),
+			'0.1',
+			true
+		);
+	}
+
+	public function output_get( $url, $link, $current ) {
+		return sprintf(
+			'<option value="%s"%s>%s</option>',
+			$url,
+			( $current ? ' selected="selected"' : '' ),
+			$link
+		);
+	}
+
+	public function output_get( $tags ) {
+			$tags = array(
+			'before_item'   => '',
+			'after_item'    => '',
+			'before_output' => '<select id="msls_languages">',
+			'after_output'  => '</select>',
+		);
+	}
+
+}
+$mslsselect = new MslsSelect(); 
---
+++ readme.txt
@@ -0,0 +1,29 @@
+﻿=== MslsSelect ===
+
+Contributors: realloc
+Donate link: http://www.greenpeace.org/international/
+Tags: multilingual, multisite, language, switcher, international, localization, i18n, menu, select-box, html select
+Requires at least: 3.6.1
+Tested up to: 4.1
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Transforms the output of the Multisite Language Switcher to an HTML select
+
+== Description ==
+
+Most people are likely to use some lines of PHP or the widget provided by the [Multisite Language Switcher](http://wordpress.org/plugins/multisite-language-switcher/) to integrate the links to the translations in their blogs.
+
+If you look for an easy way to change the output of the **Multisite Language Switcher** as an HTML select with some JavaScript that forwards the visitor automatically after selecting an option then give **MslsSelect** a try.
+
+== Installation ==
+
+* Download the plugin and uncompress it with your preferred unzip programme
+* Copy the entire directory in your plugin directory of your WordPress blog (/wp-content/plugins)
+* Activate the plugin and see the magic ;)
+
+== Changelog ==
+
+= 0.1 =
+* first version
